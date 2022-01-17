c008b555110b388d7d8821fd26399f7780b38d9e
Dan Nguyen <pedantic@gmail.com>
2022-01-16 22:38:47 -0600

    Switch to poetry pyproject

--- sunrise.py
+++ sunrise.py
@@ -19,24 +19,26 @@ def main():
 
     bulb.set_power("on")
     bulb.set_color(RED, 10, True)
-    sunrise(bulb, 20*60, 100)
+    sunrise(bulb, 20 * 60, 100)
 
 
 def sunrise(bulb, duration_secs, max_brightness):
-    colors = [[65535, 65535, 0, 3500],  # RED
-              [6500,  65535, 0, 3500],  # ORANGE
-              [9000,  65535, 0, 3500],  # YELLOW
-              [58275, 0,     0, 1500],
-              [58275, 0,     0, 2000],
-              [58275, 0,     0, 2500],
-              [58275, 0,     0, 3000],
-              [58275, 0,     0, 3500],
-              [58275, 0,     0, 4000],
-              [58275, 0,     0, 4500],
-              [58275, 0,     0, 5000],
-              [58275, 0,     0, 5500],
-              [58275, 0,     0, 6000],
-              [58275, 0,     0, 6500]]
+    colors = [
+        [65535, 65535, 0, 3500],  # RED
+        [6500, 65535, 0, 3500],  # ORANGE
+        [9000, 65535, 0, 3500],  # YELLOW
+        [58275, 0, 0, 1500],
+        [58275, 0, 0, 2000],
+        [58275, 0, 0, 2500],
+        [58275, 0, 0, 3000],
+        [58275, 0, 0, 3500],
+        [58275, 0, 0, 4000],
+        [58275, 0, 0, 4500],
+        [58275, 0, 0, 5000],
+        [58275, 0, 0, 5500],
+        [58275, 0, 0, 6000],
+        [58275, 0, 0, 6500],
+    ]
 
     time_slice = duration_secs / len(colors)
     power_step = int((65535 * max_brightness / 100) / len(colors))
@@ -44,7 +46,7 @@ def sunrise(bulb, duration_secs, max_brightness):
     power = 0
     for color in colors:
         power = power + power_step
-        color[2] = power # override the brightness
+        color[2] = power  # override the brightness
         bulb.set_color(color, time_slice * 1000, False)
         sleep(time_slice)
 
